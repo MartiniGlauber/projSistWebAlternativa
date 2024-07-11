8a27e1a565f52cbf6e2b333fdd8133748510afad
MartiniGlauber <gmartinisantos@gmail.com>
2024-07-11 16:37:03 -0300

    organizando CSS ambiente interno

--- src/db/alternativa_autolocadora.sql
+++ src/db/alternativa_autolocadora.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE alternativa_autolocadora;
+-- Criar o banco de dados se não existir
+CREATE DATABASE IF NOT EXISTS alternativa_autolocadora;
 
+-- Usar o banco de dados criado
 USE alternativa_autolocadora;
 
-CREATE TABLE endereco (
+-- Criar a tabela endereco
+CREATE TABLE IF NOT EXISTS endereco (
     endereco_id INT AUTO_INCREMENT PRIMARY KEY,
     rua VARCHAR(100) NOT NULL,
     numero VARCHAR(10) NOT NULL,
@@ -14,7 +17,38 @@ CREATE TABLE endereco (
     pais VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE cliente (
+-- Criar a tabela situacao
+CREATE TABLE IF NOT EXISTS situacao (
+    situacao_id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(50) NOT NULL
+);
+
+-- Inserir valores iniciais na tabela situacao se não existirem
+INSERT INTO situacao (descricao) 
+SELECT * FROM (SELECT 'disponível' AS descricao) AS tmp
+WHERE NOT EXISTS (SELECT descricao FROM situacao WHERE descricao = 'disponível')
+UNION ALL
+SELECT * FROM (SELECT 'alugado' AS descricao) AS tmp
+WHERE NOT EXISTS (SELECT descricao FROM situacao WHERE descricao = 'alugado')
+UNION ALL
+SELECT * FROM (SELECT 'manutenção' AS descricao) AS tmp
+WHERE NOT EXISTS (SELECT descricao FROM situacao WHERE descricao = 'manutenção');
+
+-- Criar a tabela veiculo
+CREATE TABLE IF NOT EXISTS veiculo (
+    veiculo_id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    ano INT NOT NULL,
+    placa VARCHAR(10) NOT NULL UNIQUE,
+    cor VARCHAR (50) NOT NULL,
+    valor_diaria DECIMAL(10, 2) NOT NULL,
+    situacao_id INT,
+    FOREIGN KEY (situacao_id) REFERENCES situacao(situacao_id) DEFAULT 1 -- 1 representando 'disponível'
+);
+
+-- Criar a tabela cliente
+CREATE TABLE IF NOT EXISTS cliente (
     cliente_id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf_cnpj VARCHAR (100) NOT NULL,
@@ -25,17 +59,8 @@ CREATE TABLE cliente (
     FOREIGN KEY (endereco_id) REFERENCES endereco(endereco_id)
 );
 
-CREATE TABLE veiculo (
-    veiculo_id INT AUTO_INCREMENT PRIMARY KEY,
-    marca VARCHAR(50) NOT NULL,
-    modelo VARCHAR(50) NOT NULL,
-    ano INT NOT NULL,
-    placa VARCHAR(10) NOT NULL UNIQUE,
-    cor VARCHAR (50) NOT NULL,
-    valor_diaria DECIMAL(10, 2) NOT NULL
-);
-
-CREATE TABLE locacao (
+-- Criar a tabela locacao
+CREATE TABLE IF NOT EXISTS locacao (
     locacao_id INT AUTO_INCREMENT PRIMARY KEY,
     cliente_id INT,
     veiculo_id INT,
@@ -46,7 +71,8 @@ CREATE TABLE locacao (
     FOREIGN KEY (veiculo_id) REFERENCES veiculo(veiculo_id)
 );
 
-CREATE TABLE pagamento (
+-- Criar a tabela pagamento
+CREATE TABLE IF NOT EXISTS pagamento (
     pagamento_id INT AUTO_INCREMENT PRIMARY KEY,
     locacao_id INT,
     data_pagamento DATE NOT NULL,
@@ -55,13 +81,15 @@ CREATE TABLE pagamento (
     FOREIGN KEY (locacao_id) REFERENCES locacao(locacao_id)
 );
 
-CREATE TABLE perfis_acesso (
+-- Criar a tabela perfis_acesso
+CREATE TABLE IF NOT EXISTS perfis_acesso (
     perfil_id INT AUTO_INCREMENT PRIMARY KEY,
     nome_perfil VARCHAR(50) NOT NULL,
     descricao VARCHAR(255)
 );
 
-CREATE TABLE usuarios (
+-- Criar a tabela usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
     usuario_id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
